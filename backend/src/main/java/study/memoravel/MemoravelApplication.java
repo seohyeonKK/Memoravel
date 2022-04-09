2f6454051464c26658d03a9178b6f69aa73a70b6
chanbroK <koc0819@gmail.com>
2022-04-10 03:52:10 +0900

    init spring project

---
+++ backend/src/main/java/study/memoravel/MemoravelApplication.java
@@ -0,0 +1,13 @@
+package study.memoravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoravelApplication.class, args);
+	}
+
+}
