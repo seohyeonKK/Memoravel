26599f82aed6b2edcde2cf383cd7b972c40e6d36
seohyeonKK <sts05158@gmail.com>
2022-04-11 21:04:41 +0900

    add unused-imports

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -8,22 +8,12 @@
 
 import React from 'react'
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native'
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen'
+
+
 
 const App = () => {
 
@@ -34,24 +24,5 @@ const App = () => {
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-})
 
 export default App
