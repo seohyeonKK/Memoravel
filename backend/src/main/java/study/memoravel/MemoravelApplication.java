f535e77c977e79f48697679a9b36bf6340e5653f
chanbroK <koc0819@gmail.com>
2022-04-14 01:41:34 +0900

    docs(build.gradle): remove swagger because can not use swagger2 in spring2.6.6

--- backend/src/main/java/study/memoravel/MemoravelApplication.java
+++ backend/src/main/java/study/memoravel/MemoravelApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MemoravelApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MemoravelApplication.class, args);
 	}
-
 }
