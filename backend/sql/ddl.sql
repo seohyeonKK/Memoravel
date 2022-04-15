bf4f18c1ec9b8b3b6f95c25e1658623e9bcf0278
chanbroK <koc0819@gmail.com>
2022-04-15 20:12:35 +0900

    feat(user.java): add User Table Entity class

--- backend/sql/ddl.sql
+++ backend/sql/ddl.sql
@@ -1,11 +1,11 @@
-create table users(
+create or replace table user(
     id int(10) not null auto_increment primary key,
-    userid varchar(100) not null,
-    user_name varchar(100) not null,
-    user_password varchar(500) not null,
-    phone_num varchar(500) not null,
+    userId varchar(100) not null,
+    userName varchar(100) not null,
+    userPassword varchar(500) not null,
+    phoneNum varchar(500) not null,
     email varchar(500) not null,
     gender varchar(10),
     city varchar(100),
-    reg_date datetime default now()
+    regDate datetime default now()
 );
