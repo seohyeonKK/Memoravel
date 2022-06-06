03e8c2d3236fe4b8a1d6d1815e87b920179dd9f8
seohyeonKK <sts05158@gmail.com>
2022-06-06 16:28:20 +0900

    chore: prettier 적용

--- frontend/src/pages/signup/Nickname.js
+++ frontend/src/pages/signup/Nickname.js
@@ -1,10 +1,9 @@
-import {Animated, ImageBackground, Pressable, StyleSheet, Text, TextInput, View} from "react-native"
-import React, {useEffect, useRef, useState} from 'react'
-import styles from "@/styles"
-import Images from "@assets/images"
-import {useSelector} from "react-redux"
-import {nickName} from "@/constants/language"
-
+import { Animated, ImageBackground, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
+import styles from '@/styles'
+import Images from '@assets/images'
+import { useSelector } from 'react-redux'
+import { nickName } from '@/constants/language'
 
 const Nickname = () => {
   const language = useSelector((state) => state.languageOption)
@@ -18,7 +17,7 @@ const Nickname = () => {
     Animated.timing(upAnim, {
       toValue: -50,
       duration: 1000,
-      useNativeDriver: true
+      useNativeDriver: true,
     }).start()
   }
 
@@ -26,13 +25,13 @@ const Nickname = () => {
     Animated.timing(fadeTxtAnim, {
       toValue: 1,
       duration: 1000,
-      useNativeDriver: true
+      useNativeDriver: true,
     }).start()
   }
   const fadeInInput = () => {
     Animated.timing(fadeInputAnim, {
       toValue: 1,
-      useNativeDriver: true
+      useNativeDriver: true,
     }).start()
   }
 
@@ -44,21 +43,23 @@ const Nickname = () => {
 
   return (
     <View style={styles.container}>
-      <ImageBackground source={Images.LoginBackground} resizeMode="cover"
-                       style={[styles.backgroundImg, {alignItems: 'center'}]}>
+      <ImageBackground
+        source={Images.LoginBackground}
+        resizeMode="cover"
+        style={[styles.backgroundImg, { alignItems: 'center' }]}>
         <View>
-          <Animated.View style={[nicknameStyles.info, {opacity: fadeTxtAnim}, {transform: [{translateY: upAnim}]}]}>
-            <Text
-              style={[styles.gmarketMedium, nicknameStyles.infoText, {fontWeight: "700"}]}>
+          <Animated.View
+            style={[nicknameStyles.info, { opacity: fadeTxtAnim }, { transform: [{ translateY: upAnim }] }]}>
+            <Text style={[styles.gmarketMedium, nicknameStyles.infoText, { fontWeight: '700' }]}>
               {nickName[language].infoBold}
             </Text>
-            <Text style={[styles.gmarketMedium, nicknameStyles.infoText, {fontWeight: "400"}]}>
+            <Text style={[styles.gmarketMedium, nicknameStyles.infoText, { fontWeight: '400' }]}>
               {nickName[language].infoNormal}
             </Text>
           </Animated.View>
-          <Animated.View style={[styles.longBox, {opacity: fadeInputAnim}, nicknameStyles.input]}>
+          <Animated.View style={[styles.longBox, { opacity: fadeInputAnim }, nicknameStyles.input]}>
             <TextInput
-              style={[styles.mediumText, {flex: 1, paddingLeft: 22, paddingRight: 10}]}
+              style={[styles.mediumText, { flex: 1, paddingLeft: 22, paddingRight: 10 }]}
               value={nickname}
               onChangeText={(text) => setNickname(text)}
               placeholder={nickName[language].nickname}
@@ -67,23 +68,17 @@ const Nickname = () => {
               autoCapitalize="none"
             />
             <Pressable>
-              <Text style={[styles.mediumText, {paddingRight: 19, textDecorationLine: 'underline'}]}>
+              <Text style={[styles.mediumText, { paddingRight: 19, textDecorationLine: 'underline' }]}>
                 {nickName[language].check}
               </Text>
             </Pressable>
           </Animated.View>
         </View>
-        <Text>
-          {console.log(user)}
-        </Text>
-
+        <Text>{console.log(user)}</Text>
       </ImageBackground>
-
     </View>
   )
-
 }
-
 
 const nicknameStyles = StyleSheet.create({
   info: {
@@ -95,13 +90,13 @@ const nicknameStyles = StyleSheet.create({
     alignSelf: 'baseline',
     fontSize: 18,
     lineHeight: 18,
-    color: "#FFFFFF"
+    color: '#FFFFFF',
   },
   input: {
     width: 280,
     top: -50,
     marginTop: 12,
-    alignContent: 'center'
-  }
+    alignContent: 'center',
+  },
 })
 export default Nickname
