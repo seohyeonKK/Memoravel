46a384f5538d9cf12822636a2754bd0fdb5e25a7
seohyeonKK <sts05158@gmail.com>
2022-08-29 18:19:37 +0900

    feat: 로고 이미지 추가 및 기본 프로필 사진 위치 변경

--- frontend/assets/images.tsx
+++ frontend/assets/images.tsx
@@ -1,6 +1,8 @@
 const images = {
   LoginBackground: require('@assets/img/login_background.png'),
   Logo: require('@assets/img/logo.png'),
+  ColorLogo: require('@assets/img/color_logo.png'),
+  DefaultProfileImg: require('@assets/img/noProfile.png'),
 }
 
 export default images
